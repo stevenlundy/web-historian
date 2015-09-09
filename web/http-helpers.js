f6673fb4d57b58250cd9ce1d28699bf567fedf8f
stevenlundy <steven.lundy@gmail.com>
2015-09-09 12:00:37 -0700

    Fixed quote inconsistency

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -3,11 +3,11 @@ var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
 
 exports.headers = headers = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10, // Seconds.
-  'Content-Type': "text/html"
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10, // Seconds.
+  'Content-Type': 'text/html'
 };
 
 exports.serveAssets = function(res, asset, callback) {
--- web/initialize.js
+++ web/initialize.js
@@ -9,15 +9,15 @@ module.exports = function (basePath) {
   }
 
   // if the file doesn't exist, create it.
-  if (!fs.existsSync(basePath + "/sites.txt")) {
+  if (!fs.existsSync(basePath + '/sites.txt')) {
     // We use fs.openSync to create the file
-    var file = fs.openSync(basePath + "/sites.txt", "w");
+    var file = fs.openSync(basePath + '/sites.txt', 'w');
     fs.closeSync(file);
   }
 
   // if the folder doesn't exist, create it.
-  if (!fs.existsSync(basePath + "/sites")) {
+  if (!fs.existsSync(basePath + '/sites')) {
     // We use fs.mkdirSync to create the folder
-    fs.mkdirSync(basePath + "/sites");
+    fs.mkdirSync(basePath + '/sites');
   }
 };
